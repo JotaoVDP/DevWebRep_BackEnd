d4e76409b00c0ff16c11eb96345fa8cf820f0035
JotaoVDP <153566066+JotaoVDP@users.noreply.github.com>
2024-08-28 08:45:10 -0300

    Add files via upload

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const app = require('./src/app');
-const port = process.env.PORT || 8000;
+const port = 8000;
 
 app.listen(port, () => {
     console.log('aplicaçao funfando braba na porta', port);
